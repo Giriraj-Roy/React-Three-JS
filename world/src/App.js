bbd6a5e952610b4f9aff79824bfe7bf4127df69a
Giriraj Roy <girirajroy2001@gmail.com>
2022-05-19 09:33:55 +0530

    Files Organized

---
+++ world/src/App.js
@@ -0,0 +1,26 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+import './App.css'
+
+function Box(){
+  return(
+    <mesh>
+      <boxBufferGeometry  attach="geometry" />
+      <meshLambertMaterial attach="material"  color="red" />
+    </mesh>
+  )
+}
+
+function App() {
+  return (
+    <Canvas>
+      <OrbitControls/>
+      <ambientLight intensity={0.5}/>
+      <spotLight position={[10,15,10]} angle={0.3} />
+      <Box/>
+    </Canvas>
+  )
+}
+
+export default App
