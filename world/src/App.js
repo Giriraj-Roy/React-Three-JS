235c845339b38f68ec1964639c934af09631015d
Giriraj Roy <girirajroy2001@gmail.com>
2022-05-23 10:13:15 +0530

    Components sorted

--- world/src/App.js
+++ world/src/App.js
@@ -9,21 +9,22 @@ import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry'
 import macondo_regular from './assets/Macondo_Regular.json'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
 import {useInterval} from 'usehooks-ts'
+import {AnimatedBox, AnimatedBox2} from './components/AnimateBox'
 
 extend({ TextGeometry })
 const font = new FontLoader().parse(macondo_regular);
 
 // function Sphere({props}){
 
-//   // const [hover, setHover] = useState(false)
+//    const [hover, setHover] = useState(false)
 
 //   const sphRef = useRef();
 
 //   useFrame( ({clock}) => {
-//     // sphRef.current.rotation.x += 0.01;
+//     sphRef.current.rotation.x += 0.01;
 //     sphRef.current.rotation.y += 0.01;
-//     // sphRef.current.rotation.z += 0.1;
-//     // sphRef.current.scale += 0.1;
+//     sphRef.current.rotation.z += 0.1;
+//     sphRef.current.scale += 0.1;
 //     sphRef.current.position.y = -1-Math.sin(clock.getElapsedTime())/4;
 //   })
 
@@ -34,133 +35,53 @@ const font = new FontLoader().parse(macondo_regular);
 //       rotation-x = {Math.PI *0.25}
 //       rotation-y = {Math.PI *0.25}
 //       rotation-z = {Math.PI *0.25}
-//       // scale = {2}
+//       scale = {2}
 //       position={[-2,-1.5,0]}
-//       // onClick = {(e) => Box()}
-//       // onPointerOver={(e) => setHover(true)}
-//       // onPointerOut={(e)=> setHover(false)}
+//       onClick = {(e) => Box()}
+//       onPointerOver={(e) => setHover(true)}
+//       onPointerOut={(e)=> setHover(false)}
 //     >
 
 //       <sphereBufferGeometry
-//         // args={[2,2,2]}
+//         args={[2,2,2]}
 //         attach="geometry" />
 //       <meshLambertMaterial attach="material"  color={"blue"} />
 //     </mesh>
 //   )
 // }
 
+// function AnimatedText({props}) {
 
-function AnimatedBox({props}) {
+//   const [state, setState] = useState(true)
+//   useInterval(() => setState((s) => !s), 1000)
 
-  const [state, setState] = useState(true)
-  useInterval(() => setState((s) => !s), 1000)
+//   const ref =  useRef();
+//   useFrame( ({clock}) => {
+//     let temp = Math.cos(clock.getElapsedTime())/30;
+//     ref.current.scale.x +=  state ? temp : -1*temp;
+//     ref.current.scale.y +=   state ? temp : -1*temp;
+//     ref.current.scale.z +=  state ? temp : -1*temp;
+//     ref.current.position.x += temp //Math.sin(clock.getElapsedTime());
+//     ref.current.position.y += temp //Math.sin(clock.getElapsedTime());
+//     ref.current.position.z += temp //Math.sin(clock.getElapsedTime());
+//     ref.current.position.x = temp;
+//     ref.current.position.y = 0.1*(Math.sqrt(ref.current.position.x * ref.current.position.x +0.01))
+//     console.log(ref.current.position.y);
+//     state ? (ref.current.position.x +=  state ? temp : -1*temp) : (ref.current.position.y +=  state ? temp : -1*temp)
 
-
-  // let t=0;
-
-  const ref =  useRef();
-  useFrame( ({clock}) => {
-    let temp =  Math.sin(clock.getElapsedTime())/50;
-    // ref.current.scale.x +=  state ? temp : -1*temp;
-    // ref.current.scale.y +=   state ? temp : -1*temp;
-    // ref.current.scale.z +=  state ? temp : -1*temp;
-    ref.current.position.x += state ? temp : -temp //Math.sin(clock.getElapsedTime());
-    // ref.current.position.y += temp //Math.sin(clock.getElapsedTime());
-    // ref.current.position.z += temp //Math.sin(clock.getElapsedTime());
-    // while(t < 1){
-    //   t += 0.01;
-    //   ref.current.position.x = 20*Math.cos(t);
-    //   ref.current.position.z = 20*Math.sin(t);
-    //
-    // }
-    // ref.current.position.y = 0.1*(Math.sqrt(ref.current.position.x * ref.current.position.x +0.01))
-
-    // state ? (ref.current.position.x +=  state ? temp : -1*temp) : (ref.current.position.y +=  state ? temp : -1*temp)
-
-  })
-  return (
-    <mesh
-    {...props}
-    ref = {ref}
-    scale={[0.1, 0.1, 0.1]}
-    >
-      <boxBufferGeometry attach="geometry"  />
-      <meshLambertMaterial attach="material"  color={state ? "white": "white" } />
-    </mesh>
-  )
-}
-function AnimatedBox2({props}) {
-
-  const [state, setState] = useState(true)
-  useInterval(() => setState((s) => !s), 1000)
-
-
-  // let t=0;
-
-  const ref =  useRef();
-  useFrame( ({clock}) => {
-    let temp =  Math.sin(clock.getElapsedTime())/50;
-    // ref.current.scale.x +=  state ? temp : -1*temp;
-    // ref.current.scale.y +=   state ? temp : -1*temp;
-    // ref.current.scale.z +=  state ? temp : -1*temp;
-    ref.current.position.y += state ? temp : -temp //Math.sin(clock.getElapsedTime());
-    // ref.current.position.y += temp //Math.sin(clock.getElapsedTime());
-    // ref.current.position.z += temp //Math.sin(clock.getElapsedTime());
-    // while(t < 1){
-    //   t += 0.01;
-    //   ref.current.position.x = 20*Math.cos(t);
-    //   ref.current.position.z = 20*Math.sin(t);
-    //
-    // }
-    // ref.current.position.y = 0.1*(Math.sqrt(ref.current.position.x * ref.current.position.x +0.01))
-
-    // state ? (ref.current.position.x +=  state ? temp : -1*temp) : (ref.current.position.y +=  state ? temp : -1*temp)
-
-  })
-  return (
-    <mesh
-    {...props}
-    ref = {ref}
-    scale={[0.1, 0.1, 0.1]}
-    >
-      <boxBufferGeometry attach="geometry"  />
-      <meshLambertMaterial attach="material"  color={state ? "white": "white" } />
-    </mesh>
-  )
-}
-
-function AnimatedText({props}) {
-
-  const [state, setState] = useState(true)
-  useInterval(() => setState((s) => !s), 1000)
-
-  const ref =  useRef();
-  useFrame( ({clock}) => {
-    let temp = Math.cos(clock.getElapsedTime())/30;
-    // ref.current.scale.x +=  state ? temp : -1*temp;
-    // ref.current.scale.y +=   state ? temp : -1*temp;
-    // ref.current.scale.z +=  state ? temp : -1*temp;
-    // ref.current.position.x += temp //Math.sin(clock.getElapsedTime());
-    // ref.current.position.y += temp //Math.sin(clock.getElapsedTime());
-    // ref.current.position.z += temp //Math.sin(clock.getElapsedTime());
-    // ref.current.position.x = temp;
-    // ref.current.position.y = 0.1*(Math.sqrt(ref.current.position.x * ref.current.position.x +0.01))
-    // console.log(ref.current.position.y);
-    state ? (ref.current.position.x +=  state ? temp : -1*temp) : (ref.current.position.y +=  state ? temp : -1*temp)
-
-  })
-  extend({ TextGeometry })
-  return (
-    <mesh
-    {...props}
-    ref = {ref}
-    scale={[0.1, 0.1, 0.1]}
-    >
-      <textGeometry args={['ROY', {font, size:1, height:1}]}/>
-      <meshPhysicalMaterial attach="material" color="white"/>
-    </mesh>
-  )
-}
+//   })
+//   extend({ TextGeometry })
+//   return (
+//     <mesh
+//     {...props}
+//     ref = {ref}
+//     scale={[0.1, 0.1, 0.1]}
+//     >
+//       <textGeometry args={['ROY', {font, size:1, height:1}]}/>
+//       <meshPhysicalMaterial attach="material" color="white"/>
+//     </mesh>
+//   )
+// }
 
 
 // function Box({props}){
---
+++ world/src/components/AnimateBox.js
@@ -0,0 +1,84 @@
+import { useFrame } from '@react-three/fiber';
+import React, { useRef, useState } from 'react'
+import { useInterval } from 'usehooks-ts';
+
+export function AnimatedBox({props}) {
+
+    const [state, setState] = useState(true)
+    useInterval(() => setState((s) => !s), 1000)
+  
+  
+    // let t=0;
+  
+    const ref =  useRef();
+    useFrame( ({clock}) => {
+      let temp =  Math.sin(clock.getElapsedTime())/50;
+      ref.current.scale.x +=  state ? temp/10 : -1*temp/10;
+      ref.current.scale.y +=   state ? temp/10 : -1*temp/10;
+      ref.current.scale.z +=  state ? temp/10 : -1*temp/10;
+      ref.current.position.x += state ? temp : -temp //Math.sin(clock.getElapsedTime());
+      // ref.current.position.y += temp //Math.sin(clock.getElapsedTime());
+      // ref.current.position.z += temp //Math.sin(clock.getElapsedTime());
+      // while(t < 1){
+      //   t += 0.01;
+      //   ref.current.position.x = 20*Math.cos(t);
+      //   ref.current.position.z = 20*Math.sin(t);
+      //
+      // }
+      // ref.current.position.y = 0.1*(Math.sqrt(ref.current.position.x * ref.current.position.x +0.01))
+  
+      // state ? (ref.current.position.x +=  state ? temp : -1*temp) : (ref.current.position.y +=  state ? temp : -1*temp)
+  
+    })
+    return (
+      <mesh
+      {...props}
+      ref = {ref}
+      scale={[0.1, 0.1, 0.1]}
+      >
+        <boxBufferGeometry attach="geometry"  />
+        <meshLambertMaterial attach="material"  color={state ? "white": "white" } />
+      </mesh>
+    )
+}
+export function AnimatedBox2({props}) {
+  
+    const [state, setState] = useState(true)
+    useInterval(() => setState((s) => !s), 1000)
+  
+  
+    // let t=0;
+  
+    const ref =  useRef();
+    useFrame( ({clock}) => {
+      let temp =  Math.sin(clock.getElapsedTime())/50;
+      ref.current.scale.x +=  state ? temp/10 : -1*temp/10;
+      ref.current.scale.y +=   state ? temp/10 : -1*temp/10;
+      ref.current.scale.z +=  state ? temp/10 : -1*temp/10;
+      ref.current.position.y += state ? temp : -temp //Math.sin(clock.getElapsedTime());
+      // ref.current.position.y += temp //Math.sin(clock.getElapsedTime());
+      // ref.current.position.z += temp //Math.sin(clock.getElapsedTime());
+      // while(t < 1){
+      //   t += 0.01;
+      //   ref.current.position.x = 20*Math.cos(t);
+      //   ref.current.position.z = 20*Math.sin(t);
+      //
+      // }
+      // ref.current.position.y = 0.1*(Math.sqrt(ref.current.position.x * ref.current.position.x +0.01))
+  
+      // state ? (ref.current.position.x +=  state ? temp : -1*temp) : (ref.current.position.y +=  state ? temp : -1*temp)
+  
+    })
+    return (
+      <mesh
+      {...props}
+      ref = {ref}
+      scale={[0.1, 0.1, 0.1]}
+      >
+        <boxBufferGeometry attach="geometry"  />
+        <meshLambertMaterial attach="material"  color={state ? "white": "white" } />
+      </mesh>
+    )
+  }
+
+export default AnimatedBox;
